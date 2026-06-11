ff2775e23bacee74f7af7911e72eb374d70f3c07
Minchu Kariappa <mkariappak@gmail.com>
2026-06-11 21:15:44 +0530

    Latest website updates

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,7 @@
 
 import axios from "axios";
 
-const apiUrl = import.meta.env.VITE_API_URL || "http://localhost:5000";
+const apiUrl = import.meta.env.VITE_API_URL || "https://mrwashwala-server.onrender.com";
 
 const API = axios.create({
   baseURL: apiUrl,
